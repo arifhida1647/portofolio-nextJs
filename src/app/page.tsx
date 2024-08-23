1da76a8053a0a34e75f5261f6c67e643a2237909
Arif <arifhida1647@gmail.com>
2024-08-23 20:29:06 +0700

    add project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -247,9 +247,6 @@ export default function Home() {
                     Aplikasi Reminder Obat dan berisi informasi mengenai kesehatan, aplikasi ini dibuat
                     dengan Express JS + HTML + Tailwind UI dan database Firebase
                   </p>
-                  <button className="border px-4 py-1 rounded-lg  border-gray-500 text-gray-300">
-                    <a href="https://project-service-two.vercel.app/">View Demo</a>
-                  </button>
                   <button className="border px-4 py-1 mt-2 rounded-lg  border-gray-500 text-gray-300">
                     <a href="https://github.com/arifhida1647/projectService.git">View Github</a>
                   </button>
@@ -339,9 +336,109 @@ export default function Home() {
                     Webiste ini untuk memesan layanan laundry secara online, dibuat menggunakan framework PHP
                     Codeigniter dan database nya MySql
                   </p>
+                  <button className="border px-4 py-1 mb-2 rounded-lg  border-gray-500 text-gray-300">
+                    <a href="https://sneakslab-admin.my.id/">View Demo</a>
+                  </button>
                   <button className="border px-4 py-1 rounded-lg  border-gray-500 text-gray-300">
                     <a href="https://github.com/arifhida1647/senaklab-frontend.git">View Github</a>
                   </button>
+                  {/* Meaty part - Meteor effect */}
+                  <Meteors number={20} />
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="">
+              <div className=" w-full relative max-w-xl">
+                <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
+                <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+                  <div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="h-2 w-2 text-gray-300"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
+                      />
+                    </svg>
+                  </div>
+
+                  <h1 className="font-bold text-xl text-white mb-4 relative z-50">
+                    Web Aplikasi Dashboard Laundry
+                  </h1>
+                  <Image
+                    width={300}
+                    alt="NextUI hero Image"
+                    src="https://portofolio-temp.vercel.app/images/porto-6.jpg"
+                  />
+                  <p className="font-normal text-base text-slate-500 my-4 relative z-50">
+                    Dashborad dibuat untuk melakukan pengelohan data pada aplikasi pemesanan laundry yang telah saya buat sebelumnya,
+                    dibuat menggunakan CodeIgniter + AdminLTE
+                  </p>
+                  
+                  <button className="border px-4 py-1 mb-2 rounded-lg  border-gray-500 text-gray-300">
+                    <a href="https://dashboard.sneakslab-admin.my.id/">View Demo</a>
+                  </button>
+                  <button className="border px-4 py-1 rounded-lg  border-gray-500 text-gray-300">
+                    <a href="https://github.com/arifhida1647/sneaklab_project.git">View Github</a>
+                  </button>
+
+
+                  {/* Meaty part - Meteor effect */}
+                  <Meteors number={20} />
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="">
+              <div className=" w-full relative max-w-xl">
+                <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
+                <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+                  <div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="h-2 w-2 text-gray-300"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
+                      />
+                    </svg>
+                  </div>
+
+                  <h1 className="font-bold text-xl text-white mb-4 relative z-50">
+                    Web Aplikasi Point Of Sale
+                  </h1>
+                  <Image
+                    width={300}
+                    alt="NextUI hero Image"
+                    src="https://portofolio-temp.vercel.app/images/porto-7.png"
+                  />
+                  <p className="font-normal text-base text-slate-500 my-4 relative z-50">
+                    Dashborad dibuat untuk melakukan Jual Beli dan pengaturan stok pada sebuah warung,
+                    dibuat menggunakan CodeIgniter + AdminLTE
+                  </p>
+                  <button className="border px-4 py-1 mb-2 rounded-lg  border-gray-500 text-gray-300">
+                    <a href="https://tokosembakoleon.my.id/">View Demo</a>
+                  </button>
+                  <button className="border px-4 py-1 rounded-lg  border-gray-500 text-gray-300">
+                    <a href="https://github.com/arifhida1647/sneaklab_project.git">View Github</a>
+                  </button>
+
+
                   {/* Meaty part - Meteor effect */}
                   <Meteors number={20} />
                 </div>
@@ -477,51 +574,6 @@ export default function Home() {
                   <button className="border px-4 py-1 rounded-lg mt-2 border-gray-500 text-gray-300">
                     <a href="https://github.com/arifhida1647/VuejsRoute.git">View Github</a>
                   </button>
-
-                  {/* Meaty part - Meteor effect */}
-                  <Meteors number={20} />
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="">
-              <div className=" w-full relative max-w-xl">
-                <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
-                <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
-                  <div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-500">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="h-2 w-2 text-gray-300"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
-                      />
-                    </svg>
-                  </div>
-
-                  <h1 className="font-bold text-xl text-white mb-4 relative z-50">
-                    Web Aplikasi Dashboard Laundry
-                  </h1>
-                  <Image
-                    width={300}
-                    alt="NextUI hero Image"
-                    src="https://portofolio-temp.vercel.app/images/porto-6.jpg"
-                  />
-                  <p className="font-normal text-base text-slate-500 my-4 relative z-50">
-                    Dashborad dibuat untuk melakukan pengelohan data pada aplikasi pemesanan laundry yang telah saya buat sebelumnya,
-                    dibuat menggunakan CodeIgniter + AdminLTE
-                  </p>
-                  <button className="border px-4 py-1 rounded-lg  border-gray-500 text-gray-300">
-                    <a href="https://github.com/arifhida1647/sneaklab_project.git">View Github</a>
-                  </button>
-
 
                   {/* Meaty part - Meteor effect */}
                   <Meteors number={20} />
